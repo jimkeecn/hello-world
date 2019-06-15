b732bce8068943a1370233a0ac05e677645a8b02
jimkeecn <jimkeecn@gmail.com>
2019-06-15 14:46:57 +1000

    create hello world.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
 const element = <h1>Hellow World</h1>;
 
 console.log(element);
+
+ReactDOM.render(element,document.getElementById('root'))
